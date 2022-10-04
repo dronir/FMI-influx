d0de2db2b0e453678b5d98f922b3a7db52c0ef25
Olli Wilkman <olli.wilkman@iki.fi>
2022-10-05 00:16:32 +0300

    Some type hints etc.

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # FMI open data to InfluxDB
 
-I wrote this because I wanted to store some weather data from the [FMI](https://www.fmi.fi/) into my own InfluxDB database, to easier compare it with some sensor values I measured myself.
+I wrote this because I wanted to store some weather data from the
+[FMI](https://www.fmi.fi/) into my own InfluxDB database, to easie
+compare it with some sensor values I measured myself.
--- upload_FMI.py
+++ upload_FMI.py
@@ -1,9 +1,9 @@
-
 import aiohttp
 import asyncio
 import aioinflux
 import toml
 import logging
+from typing import Dict, Any, Tuple, List
 from collections import defaultdict
 from datetime import datetime, timedelta
 from math import isnan
@@ -11,33 +11,34 @@ from sys import argv, exit
 from lxml import etree
 
 DEBUG_LEVELS = {
-    "debug" : logging.DEBUG,
-    "warning" : logging.WARNING,
-    "info" : logging.INFO,
-    "error" : logging.ERROR
+    "debug": logging.DEBUG,
+    "warning": logging.WARNING,
+    "info": logging.INFO,
+    "error": logging.ERROR,
 }
 
 QUERY_URL = "https://opendata.fmi.fi/wfs"
 
 DEFAULT_QUERY_PARAMS = {
-    "service" : "WFS",
-    "storedquery_id" : "fmi::observations::weather::simple",
-    "request" : "getFeature",
-    "version" : "2.0.0"
+    "service": "WFS",
+    "storedquery_id": "fmi::observations::weather::simple",
+    "request": "getFeature",
+    "version": "2.0.0",
 }
 
 
-def read_config(filename):
+def read_config(filename) -> Dict[str, Any]:
     """Load config from TOML file."""
-    with open(filename, 'r') as f:
+    with open(filename, "r") as f:
         return toml.loads(f.read())
 
-def get_timestring(T):
+
+def get_timestring(t: datetime) -> str:
     """ISO time string without decimals, end in Z, from datetime object."""
-    return T.isoformat().split(".")[0] + "Z"
+    return t.isoformat().split(".")[0] + "Z"
 
 
-async def rawdata_from_query(session, query_params):
+async def rawdata_from_query(session, query_params: Dict):
     """Make HTTP request, return text contents."""
     logging.debug(f"FMI query url: {QUERY_URL}")
     logging.debug(f"Query parameters: {query_params}")
@@ -54,17 +55,17 @@ async def rawdata_from_query(session, query_params):
         return None
 
 
-def xml_from_raw(raw_result):
+def xml_from_raw(raw_result: str) -> None:
     """Turn text content from HTTP request into XML tree."""
     xml_data = bytes(raw_result, "utf-8")
     try:
-        return etree.fromstring(xml_data) 
+        return etree.fromstring(xml_data)
     except Exception as E:
         logging.error(f"Exception while parsing XML data:\n {E}")
-        return etree.Element("root") # Return an empty XML tree.
+        return etree.Element("root")  # Return an empty XML tree.
 
 
-def values_from_xml(xml_tree):
+def values_from_xml(xml_tree) -> List:
     """Return all (time, variable, value) triplets, given XML tree."""
     # Handle namespaces properly because why not (could just wildcard them to be honest)
     ns_wfs = xml_tree.nsmap["wfs"]
@@ -73,7 +74,7 @@ def values_from_xml(xml_tree):
     return [value_from_element(m, ns_bswfs) for m in members]
 
 
-def value_from_element(member, ns="*"):
+def value_from_element(member, ns="*") -> Tuple:
     """Get one (time, variable, value) triplet from the XML element containing it."""
     time = member.find(f".//{{{ns}}}Time").text
     var = member.find(f".//{{{ns}}}ParameterName").text
@@ -86,16 +87,16 @@ def value_from_element(member, ns="*"):
         val = float(value)
     except Exception as E:
         logging.error(f"Failed to convert timestamp of value from XML: {E}")
-        return (datetime.utcnow(), var, float("NaN"))
+        return datetime.utcnow(), var, float("NaN")
     else:
-        return (t, var, val)
+        return t, var, val
 
 
-def points_from_values(influx_config, values):
+def points_from_values(influx_config: Dict[str,Any], values: List):
     """Make list of data points for AIOInflux from a list of (time, variable, value) triplets."""
 
     # Group the data by timestamp by putting it all in a dict. Skip missing (NaN) values.
-    temp = defaultdict(lambda: defaultdict(dict))
+    temp: defaultdict = defaultdict(lambda: defaultdict(dict))
     for t, var, value in values:
         if isnan(value):
             continue
@@ -104,19 +105,22 @@ def points_from_values(influx_config, values):
     # Make list of data points in the format expected by AIOInflux.
     points = []
     for t, fields in temp.items():
-        points.append({
-            "measurement" : influx_config["measurement"],
-            "time" : t,
-            "fields" : fields,
-            "tags" : influx_config.get("tags", {})
-        })
+        points.append(
+            {
+                "measurement": influx_config["measurement"],
+                "time": t,
+                "fields": fields,
+                "tags": influx_config.get("tags", {}),
+            }
+        )
     return points
-
 
 
 async def upload_influx(influx_config, points):
     """Upload a list of data points to InfluxDB."""
-    async with aioinflux.InfluxDBClient(**influx_config["connection"], ssl=True) as client:
+    async with aioinflux.InfluxDBClient(
+        **influx_config["connection"], ssl=True
+    ) as client:
         for point in points:
             try:
                 await client.write(point)
@@ -130,15 +134,15 @@ def check_config(config):
 
 
 def check_config_influx(config):
-    return ("connection" in config
+    return (
+        "connection" in config
         and "host" in config["connection"]
         and "port" in config["connection"]
         and "username" in config["connection"]
         and "password" in config["connection"]
         and "db" in config["connection"]
-        )
+    )
 
-     
 
 async def mainloop(config):
     influx_config = config["influxdb"]
@@ -165,7 +169,7 @@ async def mainloop(config):
             logging.error(f"No data received. Waiting {delay} seconds to retry.")
             await asyncio.sleep(delay)
             continue
-        
+
         logging.debug(raw_data)
 
         # 2. Parse XML and convert to data points for AIOInflux
@@ -179,10 +183,10 @@ async def mainloop(config):
             await upload_influx(influx_config, points)
         else:
             logging.warning(f"No data points found in XML.")
-        
+
         logging.info(f"Waiting for {delay} seconds...")
         await asyncio.sleep(delay)
-        
+
 
 if __name__ == "__main__":
     config = read_config(argv[1])
